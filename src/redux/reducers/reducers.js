12606b2ec3ed2d0184d776ce3400e5164af8ae2f
Alex Bazlinton <abazlinton@gmail.com>
2018-10-03 15:49:30 +0100

    add player swap tests and game started

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -3,15 +3,27 @@ import { START_GAME, TAKE_SQUARE } from "../actions";
 const startState = {
   grid: new Array(9).fill(null),
   winner: null,
-  currentPlayer: null
+  currentPlayer: null,
+  gameStarted: false,
+  gameOver: false
 }
 
 
+function isGridComplete(grid) {
+  return grid.every(square => square === 'X' || square === 'O')
+}
+
+function getNextPlayer(currentPlayer) {
+  return currentPlayer === 'X' ? 'O' : 'X'
+}
+
 function game(state = startState, action) {
   switch (action.type) {
+
     case START_GAME: {
       const newState = { ...state }
       newState.currentPlayer = 'X'
+      newState.gameStarted = true
       return newState
     }
 
@@ -19,6 +31,10 @@ function game(state = startState, action) {
       const newState = { ...state }
       if (!newState.grid[action.location]) {
         newState.grid[action.location] = newState.currentPlayer
+        newState.currentPlayer = getNextPlayer(newState.currentPlayer)
+      }
+      if (isGridComplete(newState.grid)) {
+        newState.gameOver = true
       }
       return newState
     }
--- src/redux/reducers/reducers.test.js
+++ src/redux/reducers/reducers.test.js
@@ -32,11 +32,53 @@ describe('TicTacToe', () => {
     expect(moveMadeState.grid).toEqual(['X', null, null, null, null, null, null, null, null]) 
   });
 
-  it('current player cannot take an already occupied square', () => {
+  it('current player cannot take if square occupied', () => {
     const fakeState = { grid: ['O'], currentPlayer: 'X'}
     const moveMadeState = game(fakeState, takeSquare(0))
     expect(moveMadeState.grid).toEqual(['O']) 
   });
+
+  it('switches player from X to O once move has been made', () => {
+    const fakeState = { grid: [null], currentPlayer: 'X'}
+    const moveMadeState = game(fakeState, takeSquare(0))
+    expect(moveMadeState.currentPlayer).toEqual('O') 
+  });
+
+  it('switches player from O to X once move has been made', () => {
+    const fakeState = { grid: [null], currentPlayer: 'O'}
+    const moveMadeState = game(fakeState, takeSquare(0))
+    expect(moveMadeState.currentPlayer).toEqual('X') 
+  });
+
+  it('does not switch player if square occupied', () => {
+    const fakeState = { grid: ['O'], currentPlayer: 'X'}
+    const moveMadeState = game(fakeState, takeSquare(0))
+    expect(moveMadeState.currentPlayer).toEqual('X') 
+  });
+
+  it('starts with game not started', () => {
+    const nextState = game(undefined, {})
+    expect(nextState.gameStarted).toBe(false)
+  });
+
+
+  it('keeps track of game having started', () => {
+    const initialState = game(undefined, {})
+    const nextState = game(initialState, startGame())
+    expect(nextState.gameStarted).toBe(true)
+  });
+
+  it('keeps track of game not being over', () => {
+    const initialState = game(undefined, {})
+    expect(initialState.gameOver).toBe(false)
+  });
+
+  it('keeps track of game having ended once grid is full', () => {
+    const fakeState = { grid: [null], currentPlayer: 'X'}
+    const nextState = game(fakeState, takeSquare(0))
+    expect(nextState.gameOver).toBe(true)
+  });
+
 
   
 });
